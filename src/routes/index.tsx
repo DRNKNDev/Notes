77f5a1bc6b5fb2b2721b8c70180ff729c8d4a19c
DRNKN Master <master@drnkn.dev>
2025-04-11 18:51:04 +0700

    feat(routing): Redirect default route to notes page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { MainPage } from "@/pages/MainPage";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-// This is the default route that will be displayed when the app loads
-// In our case, this will show the Notes section
-export const Route = createFileRoute('/')({
-  component: () => <MainPage />
+// This is the default route that will redirect to the notes route
+export const Route = createFileRoute('/')({  
+  beforeLoad: () => {
+    // Redirect to the notes route
+    throw redirect({ to: '/notes' })
+  }
 });
