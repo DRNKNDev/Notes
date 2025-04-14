34db1d5421146b4508603c89472439813c9fed70
DRNKN Master <master@drnkn.dev>
2025-04-14 09:36:06 +0700

    style(nav-user): add border to dropdown menu content

--- src/components/layout/nav-user.tsx
+++ src/components/layout/nav-user.tsx
@@ -91,7 +91,7 @@ export function NavUser({
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg border-muted"
             side={isMobile ? "bottom" : "right"}
             align="end"
             sideOffset={4}
